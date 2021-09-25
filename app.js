2e58d64be1cfed9d8c1cb7a84f8dac3ba5612873
mdahmedmaruf <mdahmedmaruf@gmail.com>
2021-09-25 10:11:34 +0600

    Add Some Style

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-const btnNumber = () => {};
+//global selection and variables
+const colorDivs = document.querySelectorAll('.color');
+const generateBtn = document.querySelectorAll('.generate');
+const sliders = document.querySelectorAll('input[type="range"]');
+const currentHexs = document.querySelectorAll('.color h2');
+let initialColors;
+
+//Color Generator
+function generateHex() {
+  const hexColor = chroma.random();
+  return hexColor;
+}
+
+function randomColors() {
+  colorDivs.forEach((div, index) => {
+    const hexText = div.children[0];
+    const randomColor = generateHex();
+
+    //Add the color to the background
+    div.style.backgroundColor = randomColor;
+    hexText.innerText = randomColor;
+    //check for contrast
+    checkTextContrast(randomColor, hexText);
+  });
+}
+function checkTextContrast(color, text) {
+  const luminance = chroma(color).luminance();
+  if (luminance > 0.5) {
+    text.style.color = 'black';
+  } else {
+    text.style.color = 'white';
+  }
+}
+randomColors();
